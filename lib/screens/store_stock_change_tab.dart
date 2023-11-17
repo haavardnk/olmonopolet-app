94fbe3affaa5d13f8d83d157c78146d538dca20f
Håvard Nordhus Kråkenes <haavardnk@outlook.com>
2023-11-17 21:45:17 +0800

    Move stock change store selector to separate file

--- lib/screens/store_stock_change_tab.dart
+++ lib/screens/store_stock_change_tab.dart
@@ -1,3 +1,4 @@
+import 'package:beermonopoly/widgets/stock_change/stock_change_store_select.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:provider/provider.dart';
@@ -32,7 +33,10 @@ class _StoreStockChangeTabState extends State<StoreStockChangeTab> {
           ),
         ),
         actions: [
-          storeSelect(_openDropDownProgKey, filters),
+          StockChangeStoreSelect(
+            openDropDownProgKey: _openDropDownProgKey,
+            filters: filters,
+          ),
           IconButton(
             onPressed: () {
               _openDropDownProgKey.currentState?.openDropDownSearch();
@@ -59,80 +63,6 @@ class _StoreStockChangeTabState extends State<StoreStockChangeTab> {
                   ),
                 );
         },
-      ),
-    );
-  }
-
-  Visibility storeSelect(
-      GlobalKey<DropdownSearchState<String>> _openDropDownProgKey,
-      Filter filters) {
-    return Visibility(
-      maintainState: true,
-      visible: false,
-      child: Container(
-        width: 10,
-        child: DropdownSearch<String>(
-          key: _openDropDownProgKey,
-          popupProps: PopupPropsMultiSelection.dialog(
-            showSelectedItems: true,
-            showSearchBox: true,
-            searchFieldProps: TextFieldProps(
-              decoration: InputDecoration(
-                labelText: 'Søk',
-                prefixIcon: Icon(
-                  Icons.search,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-            itemBuilder: (context, item, isSelected) {
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 8),
-                child: ListTile(
-                  title: Text(item),
-                  subtitle: Text(filters.storeList.isNotEmpty &&
-                          filters.storeList
-                                  .firstWhere((element) => element.name == item)
-                                  .distance !=
-                              null
-                      ? '${filters.storeList.firstWhere((element) => element.name == item).distance!.toStringAsFixed(0)}km'
-                      : ''),
-                ),
-              );
-            },
-            containerBuilder: (context, popupWidget) {
-              return Column(
-                children: [
-                  Consumer<Filter>(
-                    builder: (context, _, __) {
-                      return SwitchListTile(
-                        contentPadding: EdgeInsets.only(left: 12, right: 4),
-                        title: Text(
-                          'Husk valgt butikk',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        value: filters.filterSaveSettings[13]['save'],
-                        onChanged: (bool newValue) {
-                          filters.filterSaveSettings[13]['save'] = newValue;
-                          filters.saveFilterSettings();
-                        },
-                      );
-                    },
-                  ),
-                  Expanded(child: popupWidget),
-                ],
-              );
-            },
-          ),
-          items: filters.storeList.map((e) => e.name).toList(),
-          onChanged: (String? x) {
-            filters.stockChangeSelectedStore = x!;
-            filters.setStore(stock: true);
-          },
-          selectedItem: filters.stockChangeSelectedStore,
-        ),
       ),
     );
   }
---
+++ lib/widgets/stock_change/stock_change_store_select.dart
@@ -0,0 +1,89 @@
+import 'package:beermonopoly/providers/filter.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class StockChangeStoreSelect extends StatelessWidget {
+  const StockChangeStoreSelect({
+    Key? key,
+    required GlobalKey<DropdownSearchState<String>> openDropDownProgKey,
+    required this.filters,
+  })  : _openDropDownProgKey = openDropDownProgKey,
+        super(key: key);
+
+  final GlobalKey<DropdownSearchState<String>> _openDropDownProgKey;
+  final Filter filters;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      maintainState: true,
+      visible: false,
+      child: Container(
+        width: 10,
+        child: DropdownSearch<String>(
+          key: _openDropDownProgKey,
+          popupProps: PopupPropsMultiSelection.dialog(
+            showSelectedItems: true,
+            showSearchBox: true,
+            searchFieldProps: TextFieldProps(
+              decoration: InputDecoration(
+                labelText: 'Søk',
+                prefixIcon: Icon(
+                  Icons.search,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            itemBuilder: (context, item, isSelected) {
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: ListTile(
+                  title: Text(item),
+                  subtitle: Text(filters.storeList.isNotEmpty &&
+                          filters.storeList
+                                  .firstWhere((element) => element.name == item)
+                                  .distance !=
+                              null
+                      ? '${filters.storeList.firstWhere((element) => element.name == item).distance!.toStringAsFixed(0)}km'
+                      : ''),
+                ),
+              );
+            },
+            containerBuilder: (context, popupWidget) {
+              return Column(
+                children: [
+                  Consumer<Filter>(
+                    builder: (context, _, __) {
+                      return SwitchListTile(
+                        contentPadding: EdgeInsets.only(left: 12, right: 4),
+                        title: Text(
+                          'Husk valgt butikk',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        value: filters.filterSaveSettings[13]['save'],
+                        onChanged: (bool newValue) {
+                          filters.filterSaveSettings[13]['save'] = newValue;
+                          filters.saveFilterSettings();
+                        },
+                      );
+                    },
+                  ),
+                  Expanded(child: popupWidget),
+                ],
+              );
+            },
+          ),
+          items: filters.storeList.map((e) => e.name).toList(),
+          onChanged: (String? x) {
+            filters.stockChangeSelectedStore = x!;
+            filters.setStore(stock: true);
+          },
+          selectedItem: filters.stockChangeSelectedStore,
+        ),
+      ),
+    );
+  }
+}
