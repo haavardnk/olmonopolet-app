ec32b9a60f7b72606a1b9bda81c3187c46dadfce
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2025-11-30 21:57:32 +0100

    chore:  put search bar back under header bar

--- lib/screens/product_overview_tab.dart
+++ lib/screens/product_overview_tab.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart' hide SearchBar;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/filter.dart';
 import '../widgets/products/product_list.dart';
 import '../widgets/drawer/app_drawer.dart';
 import '../widgets/filters/product_filter_sheet.dart';
@@ -16,37 +19,54 @@ class ProductOverviewTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        surfaceTintColor: Colors.transparent,
-        centerTitle: true,
-        title: release != null
-            ? FittedBox(
-                fit: BoxFit.contain,
-                child: Text(
-                  release!.releaseDate != null
-                      ? toBeginningOfSentenceCase(DateFormat.yMMMMEEEEd('nb_NO')
-                          .format(release!.releaseDate!))
-                      : release!.name,
-                ),
-              )
-            : const ProductOverviewSearchBar(),
-        actions: [
-          release == null
-              ? const ProductFilterSheet()
-              : ProductOverviewReleaseSort(release!),
-        ],
-        bottom: (release != null && release!.productSelections.length > 1)
-            ? PreferredSize(
-                preferredSize: const Size.fromHeight(kToolbarHeight),
-                child: ProductOverviewReleaseProductSelection(release: release),
-              )
-            : null,
-      ),
-      drawer: release == null ? const AppDrawer() : null,
-      body: ProductList(
-        release: release,
-      ),
+    return Consumer<Filter>(
+      builder: (context, filters, child) {
+        final storeName = filters.selectedStores.isNotEmpty
+            ? filters.selectedStores.first
+            : 'Oversikt';
+
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: release != null
+                ? FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(
+                      release!.releaseDate != null
+                          ? toBeginningOfSentenceCase(
+                              DateFormat.yMMMMEEEEd('nb_NO')
+                                  .format(release!.releaseDate!))
+                          : release!.name,
+                    ),
+                  )
+                : Text(storeName),
+            actions: [
+              release == null
+                  ? const ProductFilterSheet()
+                  : ProductOverviewReleaseSort(release!),
+            ],
+            bottom: release != null
+                ? (release!.productSelections.length > 1)
+                    ? PreferredSize(
+                        preferredSize: const Size.fromHeight(kToolbarHeight),
+                        child: ProductOverviewReleaseProductSelection(
+                            release: release),
+                      )
+                    : null
+                : PreferredSize(
+                    preferredSize: Size.fromHeight(56.h),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(16.w, 0, 16.w, 12.h),
+                      child: const ProductOverviewSearchBar(),
+                    ),
+                  ),
+          ),
+          drawer: release == null ? const AppDrawer() : null,
+          body: ProductList(
+            release: release,
+          ),
+        );
+      },
     );
   }
 }
